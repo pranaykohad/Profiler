603674eb5c1da85be8a2efeac4ae12f2c1a576a8
pranaykohad <prakohad@gmail.com>
2021-01-25 20:10:20 +0530

    first commit

---
+++ ProfilerAPI/src/main/java/com/profiler/ProfilerAPI/ProfilerApiApplication.java
@@ -0,0 +1,13 @@
+package com.profiler.ProfilerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilerApiApplication.class, args);
+	}
+
+}
